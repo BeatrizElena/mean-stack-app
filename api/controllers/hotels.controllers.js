80a2b66cb0b4118a04df35320ab52b7666ac3733
Beatriz Ortiz <beatrizortiz@MacBook-Pro.local>
2017-10-02 17:20:56 -0400

    Finished session2-MongoDB

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -52,9 +52,26 @@ module.exports.hotelsGetOne = function(req, res) {
 module.exports.hotelsAddOne = function(req, res) {
     var db = dbconn.get();
     var collection = db.collection('hotels');
+    var newHotel;
+
     console.log("POST new hotel");
-    console.log(req.body); //req.body is where the body parts and middleware we've applied will put all the data it passes
-    res
-        .status(200)
-        .json(req.body);
-};
+    
+    if (req.body && req.body.name && req.body.stars) {
+        newHotel = req.body;
+        newHotel.stars = parseInt(req.body.stars, 10);
+        collection.insertOne(newHotel, function(err, response) {
+            console.log(response); 
+            console.log(response.ops);
+            res
+                .status(201) //201 is the status code when a new resource is created
+                .json(response.ops);
+        });
+        
+    } else {
+        console.log("Data missing from body");
+        res
+            .status(400)
+            .json({message : "Required data missing from body"});
+    }
+    
+    };
