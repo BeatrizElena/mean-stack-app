3cc67f649e8148afb56819a21558956b2892246f
Beatriz Ortiz <beatrizortiz@MacBook-Pro.local>
2017-10-18 09:00:14 -0400

    AngulaJS-Vid40-SPAs, Routes & Templates

--- public/app.js
+++ public/app.js
@@ -1,2 +1,17 @@
-angular.module('myApp', []) // Module Setter Syntax: array is for dependencies but even w/out dependencies an empty array is needed
+ // Module Setter Syntax: array is for dependencies but even w/out dependencies an empty array is needed
 // controllers are in separate file: controllers.js which we'll link to in the html file. 
+angular.module('myApp', ['ngRoute']).config(config);
+
+function config($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'templates/main.html',
+        controller: 'MyController',
+        controllerAs: 'vm'
+    }).when('/about', {
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController',
+        controllerAs: 'vm'
+    }).otherwise({
+        redirectTo: '/'
+    });
+}
--- public/controller.js
+++ public/controller.js
@@ -1,7 +1,13 @@
  // Module Getter Syntax -- angular.module('myApp'); -- gets a module "myApp" to which you can add -- .controller(.'MyController', MyController) -- 
  // where "MyController" will be handled by a function called MyController.
- angular.module('myApp').controller('MyController', MyController); 
+ angular.module('myApp').controller('MyController', MyController).controller('AboutController', AboutController); 
+ 
  function MyController() {
      var vm = this;
      vm.name = "Bea";
+ }
+
+ function AboutController() {
+     var vm = this;
+     vm.about = "This is my bio";
  }
