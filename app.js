c4c5d9ccece1bab1f80f8e24b88339c3db386319
Beatriz Ortiz <beatrizortiz@MacBook-Pro.local>
2017-10-19 09:42:13 -0400

    hotelApp1: added static route & module for 'meanhotel' app, config route to 'hotel.html', wrote HotelController for loading title from 'hotel.html'.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// code in this file relates to the nodejS part of the code. 
+// This code goes out to the DB and handles the various http requests.
+
 require('./api/data/db.js'); //as soon as app starts, connect to DB (through mongoose connection).
 var express = require('express');
 var app = express(); //initializes express to bring it into the application
@@ -8,20 +11,21 @@ var routes = require('./api/routes'); // require the routes folder
 
 app.set('port', 3000); // sets the port
 
-// Using middleware code to console.log a log of when any resource gets requested. Where the code is placed is IMPORTANT!
+// add middleware code to console.log every request. Where the code is placed is IMPORTANT!
 app.use(function(req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
-app.use(express.static(path.join(__dirname, 'public'))); //this replaces 4-5 lines of code needed w/ app.get()
-// for bodyParser, we'll choose urlencoded bc that's how the html form is sent. Other options are raw or json but we don't need those now.
-// within the urlencoded method, we need to specify an extended option, which is set as an object and 
-// we're choosing to set it to false. False gives us strings and arrays in our form body.
-// Setting it to true can give us access to other data types but we don't need that now.
+// set static directories before defining routes, so that browser can access files in 'public' and 'node_modules' folders.
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
+
+// enable parsing of posted forms.
 app.use(bodyParser.urlencoded({ extended : false}));
 app.use('/api', routes); //tell express to use the routes folder, from the api folder
 
+// listen for requests
 var server = app.listen(app.get('port'), function(){
     var port = server.address().port; //extracts the port # from the server object
     console.log("Magic happens on port " + port);
