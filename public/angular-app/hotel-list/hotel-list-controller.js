0b3d8ad00e678aee8e5e920a222773f483fe29fd
Beatriz Ortiz <beatrizortiz@MacBook-Pro.local>
2017-10-19 11:52:02 -0400

    hotelApp3: added id route, HotelController & hotel.html to display info from that route, and hotelDataFactory to streamline $http functions.

--- public/angular-app/hotel-list/hotel-list-controller.js
+++ public/angular-app/hotel-list/hotel-list-controller.js
@@ -1,9 +1,9 @@
 angular.module('meanhotel').controller('HotelsController', HotelsController);
 
-function HotelsController($http) {
+function HotelsController(hotelDataFactory) {
     var vm = this;
     vm.title = 'MEAN Hotel App';
-    $http.get('api/hotels?count=10').then(function(response) {
+    hotelDataFactory.hotelList().then(function(response) {
         // console.log(response); // we get all properties of the object
         vm.hotels = response.data; //we get just the data property of the object
     });
